d17f6d8fb28c291dd1f27f140deaf3949d2c2a5a
azeemafirdouss <azeemafirdous14@gmail.com>
2026-05-25 22:09:35 +0530

    Fix Express 5 wildcard routing using regex /.*/

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use('/api/reviews', reviewRoutes);
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 // Catch-all route to serve the React index.html
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
